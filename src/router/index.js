e04981849bd325b38574a8eaa88af27d3772d208
grothlin-iut90 <gael.rothlin@edu.univ-fcomte.fr>
2025-03-30 23:03:16 +0200

    restriction de route si non-connecté

--- src/router/index.js
+++ src/router/index.js
@@ -1,46 +1,73 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import Organisations from '../views/OrganisationsView.vue'
-import Organisation from "@/views/OrganisationView.vue";
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import OrganisationsView from "../views/OrganisationsView.vue";
+import OrganisationView from "@/views/OrganisationView.vue";
 import TeamView from "@/views/TeamView.vue";
 import TeamsView from "@/views/TeamsView.vue";
+import AuthentificationView from "@/views/AuthentificationView.vue";
+import store from "@/store";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/organisations',
-    name: 'Organisations',
-    component: Organisations
+    path: "/auth",
+    name: "auth",
+    component: AuthentificationView,
   },
   {
-    path: '/organisation',
-    name: 'Organisation',
-    component: Organisation
+    path: "/organisations",
+    name: "organisations",
+    component: OrganisationsView,
+    meta: { requiresAuth: true }, 
   },
   {
-    path: '/teams',
-    name: 'Teams',
-    component: TeamsView
+    path: "/organisation",
+    name: "organisation",
+    component: OrganisationView,
+    meta: { requiresAuth: true }, 
   },
   {
-    path: '/team',
-    name: 'Team',
-    component: TeamView
-  }
-
-]
+    path: "/teams",
+    name: "teams",
+    component: TeamsView,
+    meta: { requiresAuth: true }, 
+  },
+  {
+    path: "/team",
+    name: "team",
+    component: TeamView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "*", 
+    redirect: "/",
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    const isAuthenticated = store.state.secret.OrganisationPassword !== "";
+    if (!isAuthenticated) {
+      next({ name: "auth" });
+    } else {
+      next(); 
+    }
+  } else {
+    next(); 
+  }
+});
+
+export default router;
